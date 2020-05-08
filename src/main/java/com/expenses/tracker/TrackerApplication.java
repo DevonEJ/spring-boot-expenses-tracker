747d02f11b8fdc3952f3c4153eb6e349be41526e
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 19:12:49 +0100

    add libraries for http requests

--- src/main/java/com/expenses/tracker/TrackerApplication.java
+++ src/main/java/com/expenses/tracker/TrackerApplication.java
@@ -2,6 +2,9 @@ package com.expenses.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class TrackerApplication {
@@ -9,6 +12,7 @@ public class TrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
 	}
-	
+
+}
 
 }
