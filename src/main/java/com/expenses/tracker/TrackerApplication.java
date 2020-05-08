f5ccbff99fae4eaaa1a9ed9ffe87fc7f3a0edf10
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 20:37:35 +0100

    add logger

--- src/main/java/com/expenses/tracker/TrackerApplication.java
+++ src/main/java/com/expenses/tracker/TrackerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// import com.expenses.Expense;
+import com.expenses.Expense;
 
 
 @SpringBootApplication
@@ -23,7 +23,7 @@ public class TrackerApplication {
 	}
 
 	@GetMapping("/newexpense")
-	public String expense(@RequestParam(value = "name") String name) {
+	public String createExpense(@RequestParam(value = "name") String name) {
 	return String.format("New expense logged for %s!", name);
 }
 
