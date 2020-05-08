9a5b07d32efb7d99e6abe71a7ad15d25de4f3de3
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 20:27:41 +0100

    adding tostring override and jpa tags

--- src/main/java/com/expenses/Expense.java
+++ src/main/java/com/expenses/Expense.java
@@ -3,49 +3,66 @@ package com.expenses;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Expense {
 
-        private final UUID id;
-        private final String name;
-        private final double amount;
-        private final Date paymentDate;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private final UUID id;
+    private final String name;
+    private final double amount;
+    private final Date paymentDate;
 
-        Expense() {
-            this.id = generateExpenseId();  
-            this.name = "";
-            this.amount = 0.0;
-            this.paymentDate = new Date();
-        }
+    protected Expense() {}
 
-        Expense(String expenseName, double expenseAmount, Date paid) {
-            this.id = generateExpenseId();
-            this.name = expenseName;
-            this.amount = expenseAmount;
-            this.paymentDate = paid;
-        }
-    
-        public static UUID generateExpenseId() {
-            UUID uniqueID = UUID.randomUUID();
-            return uniqueID;
-        }
+    // Expense() {
+    //     this.id = generateExpenseId();  
+    //     this.name = "";
+    //     this.amount = 0.0;
+    //     this.paymentDate = new Date();
+    // }
 
+    Expense(String expenseName, double expenseAmount, Date paid) {
+        this.id = generateExpenseId();
+        this.name = expenseName;
+        this.amount = expenseAmount;
+        this.paymentDate = paid;
+    }
 
-        public static void main(String[] args) {
+    public static UUID generateExpenseId() {
+        UUID uniqueID = UUID.randomUUID();
+        return uniqueID;
+    }
 
-            Date date = new Date();  
-
-            Expense test = new Expense();
-            System.out.println(test.id);
-
-
-            Expense myExpense = new Expense("food", 20.00, date);
-            System.out.println(myExpense.amount);
-            System.out.println(myExpense.paymentDate);
-            System.out.println(myExpense.name);
+    @Override
+    public String toString() {
+      return String.format(
+          "Expense: [ID=%d, Name='%s', Amount='%s', PaymentDate='%s']",
+          id, name, amount, paymentDate);
+    }
 
 
-        }
-    
+    public static void main(String[] args) {
+
+        Date date = new Date();  
+
+        Expense test = new Expense();
+        System.out.println(test.id);
+
+
+        Expense myExpense = new Expense("food", 20.00, date);
+        System.out.println(myExpense.amount);
+        System.out.println(myExpense.paymentDate);
+        System.out.println(myExpense.name);
+
+
+    }
+
     
 
     
