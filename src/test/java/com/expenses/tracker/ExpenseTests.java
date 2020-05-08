acc553398273b9939b4503b9f6ef1c4cdf13892c
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 23:03:01 +0100

    add test file for expense class

---
+++ src/test/java/com/expenses/tracker/ExpenseTests.java
@@ -0,0 +1,13 @@
+package com.expenses.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
