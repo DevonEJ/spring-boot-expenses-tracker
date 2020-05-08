14c95945d34e44577afcaed784d9164165c75779
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 19:22:21 +0100

    adding outline for expense class

---
+++ src/main/java/com/expenses/Expense.java
@@ -0,0 +1,30 @@
+package com.expenses;
+
+import java.util.Date;
+
+public class Expense {
+
+        private final long id;
+        private final String name;
+        private final double amount;
+        private final Date paymentDate;
+
+        public Expense(String name, double amount, Date paid) {
+            this.id = setExpenseId();
+            this.name = name;
+            this.amount = amount;
+            this.paymentDate = paid;
+        }
+    
+        public long setExpenseId() {
+            return id;
+        }
+    
+        public long getId() {
+            return id;
+        }
+    
+    
+
+    
+}
