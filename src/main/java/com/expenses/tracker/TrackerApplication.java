6a9c322d93a3d3bf8543285548e688546c7f6b44
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 19:58:21 +0100

    adding rest controller and basic handlers

--- src/main/java/com/expenses/tracker/TrackerApplication.java
+++ src/main/java/com/expenses/tracker/TrackerApplication.java
@@ -4,15 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// import com.expenses.Expense;
 
 
 @SpringBootApplication
+@RestController
 public class TrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+	return "Welcome to the expenses logger!";
+	}
+
+	@GetMapping("/newexpense")
+	public String expense(@RequestParam(value = "name") String name) {
+	return String.format("New expense logged for %s!", name);
 }
 
 }
+
