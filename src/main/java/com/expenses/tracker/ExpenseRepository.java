df1b0941bcd7177425409fd989051d4dc1c08acb
Devon Edwards Joseph <devonedwardsjoseph@devons-mbp.home>
2020-05-08 21:16:18 +0100

    switch to using rest for calling expense repository methods

--- src/main/java/com/expenses/tracker/ExpenseRepository.java
+++ src/main/java/com/expenses/tracker/ExpenseRepository.java
@@ -1,12 +1,15 @@
 package com.expenses.tracker;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-public interface ExpenseRepository extends CrudRepository<Expense, Long> {
+@RepositoryRestResource(collectionResourceRel = "expenses", path = "expenses")
+public interface ExpenseRepository extends PagingAndSortingRepository<Expense, Long> {
 
-  List<Expense> findByName(String name);
+  List<Expense> findByName(@Param("name") String name);
 
-  Expense findById(long id);
 }
